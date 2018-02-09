c6776cc3228318cff224667119034f18d35c4d40
lollyi <322329@bsd48.org>
2018-02-08 17:01:07 -0800

    Vision in progress drawing rectangle around tape

--- vision/vtape.py
+++ vision/vtape.py
@@ -7,22 +7,50 @@ cap.set(11, 0.1) #default was .433, lower contrast is better
 print var
 
 while(1):
+	#capture an image
 	_, frame = cap.read() 
-#sets frame to equal the video capture window
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-#changes color to hsv
-	lower_green = np.array([75,100,150]) 
-#saw online somewhere that last value only goes to 180 so if problems occur with lower green...
-	upper_green = np.array([95,255,255]) #messing with hsv
+	#convert to HSV
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+	#find green pixels image using limits in in numpy arrays
+	lower_green = np.array([75,100,150]) 
+	upper_green = np.array([95,255,255])
+
+	#mask filters colors out of the green range from
+	# the frame being read
 	mask = cv2.inRange(hsv, lower_green, upper_green)
-#mask filters colors out of the green range from the frame being read
-	result = cv2.bitwise_and(frame, frame, mask = mask)
+	#cv2.imshow('mask', mask)
+
+	ret,thresh = cv2.threshold(mask,127,255,0)
+	#cv2.imshow('thresh', thresh)
+
+	# get contours in hopes that these can be converted to
+	# rectangles	
+	contours,hierarchy = cv2.findContours(thresh, 1, 2)
+	if not len(contours) > 0:
+		break
+ 
+	cnt = contours[0]
+	M = cv2.moments(cnt)
+	print M
+	print contours
+	#approximate shape of object and draw a line surrounding the
+	#object, make polygon a rectangle
+	epsilon = 0.1*cv2.arcLength(cnt,True)
+	approx = cv2.approxPolyDP(cnt,epsilon,True)
+	cv2.imshow('approx', approx)
+#ret is return value from the camera frame
+
+#uses variables to speed up process, just shows process of contouring in terminal
+	#perimeter = cv2.arcLength(cnt,True)
+	#for (x,y,w,h) in tape:
+		#cv2.rectangle(perimeter,(x,y),(x+w,y+h)(255,200,100),2)
+
+	#result = cv2.bitwise_and(frame, frame, mask = mask)
 #applies mask by saying input array 1 and 2 are frame, output array is mask (still don't fully understand why a bitwise_and is necessary)
 	
-	cv2.imshow('frame', frame)
-#	cv2.imshow('mask', mask)
-	cv2.imshow('result', result)
+	#cv2.imshow('frame', frame)
+	#cv2.imshow('result', result)
 #why can't you just show result? b/c when it doesn't work you want to know why...
 
 	k = cv2.waitKey(5) & 0xFF
