b415e5afc17d684ca39e944d90f11c87d3953f41
seanic101 <seansullivanspds@gmail.com>
2018-02-16 20:29:38 -0800

    [everything]

--- drivingcode/robot.py
+++ drivingcode/robot.py
@@ -3,11 +3,11 @@
 import wpilib
 from wpilib.buttons.joystickbutton import JoystickButton
 import drive
-
+import time
 
 class BeaverTronicsRobot(wpilib.IterativeRobot):
     def robotInit(self):
-        
+     
         #**************Camera Initialization********************
         #Networktables.initialize('server=roborio.5970-frc.local')
         #wpilib.CameraServer.launch()
@@ -30,19 +30,22 @@ class BeaverTronicsRobot(wpilib.IterativeRobot):
         self.right_motors.append(wpilib.VictorSP(3))
         self.right_motors.append(wpilib.VictorSP(5))
 
-        # too lazy to change the formal from a list even though its only 1 thing.deal with it.
         # Initialize the winch Motor
         self.Winch_motor = []
         self.Winch_motor.append(wpilib.VictorSP(8))
-
-
+        
+        #initialize the ultrasonic sensor
+        self.Ultra = wpilib.AnalogInput(3)
+        self.Gyroo = wpilib.ADXRS450_Gyro()
+        
+        
         # Initialize the pop Motor
         self.pop_motor = []
         self.pop_motor.append(wpilib.VictorSP(7))
 
         #Initialize the left and right encoders
-        self.Rcoder = wpilib.Encoder(2,3)
-        self.Lcoder = wpilib.Encoder(0,1)
+        self.Rcoder = wpilib.Encoder(3,2)
+        self.Lcoder = wpilib.Encoder(1,0)
 
 
         #***************Driverstation Initialization******************
@@ -67,19 +70,77 @@ class BeaverTronicsRobot(wpilib.IterativeRobot):
         self.auto_loop_counter = 0
         self.Lcoder.reset()
         self.Rcoder.reset()
-
+        self.stage = 0
+        
+        
     def autonomousPeriodic(self):
         #old auto code could be used to cross baseline
-        print(self.auto_loop_counter)
-        if self.auto_loop_counter < 10:
+        #print(self.auto_loop_counter)
+        #x = self.Ultra.getVoltage()
+        #self.Ultra.getVoltage() == x:
+        '''if self.auto_loop_counter < 10:
             self.setDriveMotors(.5, -.5)  # forward auto
             print("left encoder value "+str(self.Lcoder.get()))
             print("Right encoder value "+str(self.Rcoder.get()))
-        elif self.Rcoder.get() >= 2000:
+        elif self.Rcoder.get() >= 20000:
             self.setDriveMotors(0,0)
             print("got past 2000")
-        self.auto_loop_counter += 1
-    
+        if self.Rcoder.get() <= 2000:
+            self.setDriveMotors(.5,-.5)
+        elif self.Ultra.getVoltage() <= 1:
+            if self.Rcoder.get() >= 6000:
+                self.setDriveMotors(.3,-.3)'''
+        print(self.Ultra.getVoltage())
+        
+        
+        if self.stage ==0:
+            self.setDriveMotors(0,0)
+            self.Gyroo.calibrate()
+            self.stage =1
+        
+        
+        
+        elif self.stage ==1:
+            if self.Rcoder.get() >= 300:
+                self.setDriveMotors(-.25,.25)
+            else:
+                self.Rcoder.reset()
+                self.stage = 2
+        
+        elif self.stage ==2:
+            self.setDriveMotors(-.25,-.25)
+            x=self.Gyroo.getAngle()
+            if int(x) >= -181:
+                self.Gyroo.reset()
+                self.stage = 3
+                
+        
+        
+        elif self.stage ==3:
+            self.setDriveMotors(-.35,.35)
+            if self.Ultra.getVoltage() <= .7:
+                self.stage =1
+                self.Rcoder.reset()
+        '''if self.stage ==1:
+            if self.Rcoder.get() <= 4000:
+                self.setDriveMotors(.5,-.5)
+            else:
+                self.stage = 2
+        
+        elif self.stage ==2:
+            self.setDriveMotors(.25,-.25)
+            if self.Ultra.getVoltage() >= 1:
+                self.stage = 3
+        
+        
+        elif self.stage ==3:
+            self.setDriveMotors(0,0)
+            #if self.Ultra.getVoltage() >= 1:
+                #self.stage = 2
+        
+        '''
+        
+            
 
 
 
@@ -108,16 +169,15 @@ class BeaverTronicsRobot(wpilib.IterativeRobot):
         self.drivetrainMotorControl()
         self.Winch()
         self.Pop()
-        
     
     def testPeriodic(self):
         """This function is called periodically during test mode."""
    
 
     def drivetrainMotorControl(self):
-            left = self.steering.getY()
-            right = self.throttle.getY()
-            drive_powers = drive.tankdrive(right, left)
+            right = self.steering.getY()
+            left = self.throttle.getY()
+            drive_powers = drive.tankdrive(right*-1, left*-1)
             self.leftspeeds = drive_powers[0]
             self.rightspeeds = drive_powers[1]
             # set the motors to powers
